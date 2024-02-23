b5e15ba110d095641a29cefe31a77592d6fc253d
Isaiah Hayes <isaiahwhayes@gmail.com>
2024-02-22 19:20:03 -0800

    login functions automated for valid users and locked out users

---
+++ cypress/e2e/homepage/login.cy.js
@@ -0,0 +1,43 @@
+/// <reference types = "cypress" />
+
+beforeEach('visit the suace demo website', function () {
+    cy.visit('/')
+});
+
+// successful login as a standard user
+describe('login as standard user', function () {
+    
+    it('adds the "standard user" credentials', function () {
+
+        // login with valid credentials
+        cy.get('#user-name').click()
+        .type('standard_user')
+        
+        cy.get('#password').click()
+        .type('secret_sauce{enter}')
+
+        // verify the correct URL redirect
+        cy.url().should('include', '/inventory.html')
+        cy.get('.app_logo').should('contain', "Swag Labs")
+    });
+});
+
+
+// attempt to login as a locked out user
+describe('login as a locked out user', function () {
+    it("blocks the locked out user from signing in", function () {
+
+        // login with the correct credentials for this locked out user
+        cy.get('#user-name').click()
+        .type('locked_out_user')
+
+        cy.get('#password').click()
+        .type('secret_sauce{enter}')
+
+        // verify the error messages
+        cy.get('h3[data-test="error"]')
+        .should('contain', "Epic sadface: Sorry, this user has been locked out");
+    });
+});
+
+// 
